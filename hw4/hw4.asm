24c94cf87838fe475b4c40e6feddf4a5a5a00273
AlamTahmidul <mohammed.alam.217@gmail.com>
2021-04-23 15:59:51 -0400

    hw4: part6 done

--- hw4/hw4.asm
+++ hw4/hw4.asm
@@ -74,21 +74,30 @@ create_person: # $s0 = *network
 	# Node* create_person(Network* ntwrk)
 	move $s0, $a0
 
-	lw $t0, 16($s0) # Gets the current number of nodes
-	lw $t1, 0($s0) # Gets the total number of nodes
-	beq $t0, $t1, cp_full_nodes
+	# Check if current > total
+	lw $t0, 16($s0) # Nodes -> 0
+	lw $t1, 0($s0)
+	bge		$t0, $t1, cp_full_nodes	# if $t0 > $t1 then err
+	bltz $t0, cp_full_nodes
+	
+	lw $t0, 20($s0) # Edges -> 4
+	lw $t1, 4($s0)
+	bgt	$t0, $t1, cp_full_nodes	# if $t0 > $t1 then err
+	bltz $t0, cp_full_nodes
 
-	# $t0 has the current number of nodes
 	# index location of newly added node = 36 + (cur_num_of_nodes * size_of_node)
+	lw $t0, 16($s0) 	# $t0 has the current number of nodes
 	lw $t1, 8($s0) # Gets the size_of_node
 	mult	$t0, $t1			# $t0 * $t1 = Hi and Lo registers; cur_num_of_nodes * size_of_node
 	mflo	$t0					# copy Lo to $t0; $t0 holds the product
 	addi $t0, $t0, 36 # Go to the empty node
 
 	add $v0, $s0, $t0 # Return the sum of address + location of empty node
-	lbu $t0, 16($s0) # Get curr_num_of_nodes 
+	
+	lw $t0, 16($s0) # Get curr_num_of_nodes 
 	addi $t0, $t0, 1 # Increase num. of current nodes by 1 
-	sb $t0, 16($s0) # Update the network
+	sw $t0, 16($s0) # Update the network
+
 	jr $ra
 	cp_full_nodes:
 		li $v0, -1 # Nodes are full
@@ -97,6 +106,17 @@ is_person_exists: # $s0 = *ntwrk, $s1 = person
 	# int is_person_exists(Network* ntwrk, Node* person)
 	move $s0, $a0
 	move $s1, $a1 # Person
+
+	# Check if current > total
+	lw $t0, 16($s0) # Nodes -> 0
+	lw $t1, 0($s0)
+	bgt		$t0, $t1, not_found_is_person	# if $t0 > $t1 then not_found_is_person
+	bltz $t0, not_found_is_person # Or if current_num_of_nodes is an invalid input
+	
+	lw $t0, 20($s0) # Edges -> 4
+	lw $t1, 4($s0)
+	bgt	$t0, $t1, not_found_is_person	# if $t0 > $t1 then not_found_is_person
+	bltz $t0, not_found_is_person
 
 	li $t4, 0 # Counter
 	loop_is_person:
@@ -112,19 +132,6 @@ is_person_exists: # $s0 = *ntwrk, $s1 = person
 		add $t0, $s0, $t0 # Addr += location
 		addi $t0, $t0, 36 # Go to the node
 		beq $t0, $s1, exit_loop_is_person # If the addresses are the same, then return 1
-
-		# addi $a0, $t0, 36 # Go to the node
-		# move $a1, $s1
-		
-		# addi $sp, $sp, -4
-		# sw $ra, 0($sp)
-
-		# jal str_equals
-
-		# lw $ra, 0($sp)
-		# addi $sp, $sp, 4
-
-		# bgtz $v0, exit_loop_is_person # Person matches
 		
 		addi $t4, $t4, 1 # Increase counter
 		j loop_is_person
@@ -135,19 +142,62 @@ is_person_exists: # $s0 = *ntwrk, $s1 = person
 	not_found_is_person:
 		li $v0, 0
 		jr $ra
-is_person_name_exists: # $s0 = *ntwrk, $s1 = name
+is_person_name_exists: # $s0 = *ntwrk, $s1 = name, $s2 = copy of counter, $s3 = copy of address
 	# int is_person_name_exists(Network* ntwrk, char* name)
 	move $s0, $a0
 	move $s1, $a1
 
+	# Check if current > total
+	lw $t0, 16($s0) # Nodes -> 0
+	lw $t1, 0($s0)
+	bgt	$t0, $t1, exit_loop_is_person_name	# if $t0 > $t1 then exit_loop_is_person_name
+	bltz $t0, exit_loop_is_person_name # Or if current_num_of_nodes is an invalid input
+	
+	lw $t0, 20($s0) # Edges -> 4
+	lw $t1, 4($s0)
+	bgt	$t0, $t1, exit_loop_is_person_name	# if $t0 > $t1 then exit_loop_is_person_name
+	bltz $t0, exit_loop_is_person_name
+
 	li $t4, 0 # Counter
 	loop_is_person_name:
+		lw $t0, 16($s0) # Get current number of nodes
+		beq $t4, $t0, exit_loop_is_person_name
 
+		# 36 + index*size_of_node
+		lw $t0, 8($s0) # Size of ndoe
+		mult	$t0, $t4			# $t0 * $t4 = Hi and Lo registers
+		mflo	$t0					# copy Lo to $t0; product of index*size_of_node
+		add $t3, $s0, $t0 # Addr + location of node
+		addi $t3, $t3, 36 # offset by 36 to get the node
+
+		move $s2, $t4 # save a copy of $t4
+		move $s3, $t3 # Save a copy of the address	
+		addi $sp, $sp, -4
+		sw $ra, 0($sp)
+
+		move $a0, $t3
+		move $a1, $s1
+		jal str_equals
+
+		lw $ra, 0($sp)
+		addi $sp, $sp, 4
+		
+		move $t4, $s2 # Restore $t4
+
+		bgtz $v0, is_person_name_found
+
+		addi $t4, $t4, 1 # Increase counter
 		j loop_is_person_name
 	exit_loop_is_person_name:
-		# Return value here
+		# Return 0 because name not found
+		li $v0, 0
+		jr $ra
+	is_person_name_found:
+		li $v0, 1
+		move $v1, $s3
 		jr $ra
 add_person_property:
+	
 	jr $ra
 get_person:
 	jr $ra
--- hw4/is_person_name_exists_test.asm
+++ hw4/is_person_name_exists_test.asm
@@ -2,7 +2,8 @@
 # Test your code with different Network layouts
 # Don't assume that we will use the same layout in all our tests
 .data
-Name1: .asciiz "Cacophonix"
+# Name1: .asciiz "Cacophonix"
+Name1: .asciiz "John Doe"
 Name2: .asciiz "Cacphonix"
 Name_prop: .asciiz "NAME"
 
@@ -11,32 +12,64 @@ Network:
   .word 10  #total_edges (bytes 4- 7)
   .word 12  #size_of_node (bytes 8 - 11)
   .word 12  #size_of_edge (bytes 12 - 15)
-  .word 0   #curr_num_of_nodes (bytes 16 - 19)
+  .word 5   #curr_num_of_nodes (bytes 16 - 19)
   .word 0   #curr_num_of_edges (bytes 20 - 23)
   .asciiz "NAME" # Name property (bytes 24 - 28)
   .asciiz "FRIEND" # FRIEND property (bytes 29 - 35)
    # nodes (bytes 36 - 95)	
-  .byte 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0	
+  .byte 'J' 'a' 'n' 'e' ' ' 'D' 'o' 'e' '\0' 0 0 0 'J' 'o' 'h' 'n' ' ' 'D' 'o' 'e' '\0' 0 0 0 'O' 't' 'h' 'e' 'r' ' ' 'D' 'o' 'e' '\0' 0 0 'C' 'a' 'c' 't' 'u' 's' '\0' 0 0 0 0 0 'S' 't' 'a' 'r' 't' '\0' 0 0 0 0 0 0	
    # set of edges (bytes 96 - 215)
   .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
 
 .text:
 main:
-	la $a0, Network
-	jal create_person
-	move $s0, $v0
+	# la $a0, Network
+	# jal create_person
+	# move $s0, $v0
+	
+	# la $a0, Network
+	# move $a1, $s0
+	# la $a2, Name_prop
+	# la $a3, Name1
+	# jal add_person_property
+	
+	# la $a0, Network
+	# la $a1, Name2
+	# jal is_person_name_exists 
+	
+	# la $a0, Network
+	# addi $a0, $a0, 36
+	# li $v0, 4
+	# syscall
 	
 	la $a0, Network
-	move $a1, $s0
-	la $a2, Name_prop
-	la $a3, Name1
-	jal add_person_property
-	
-	la $a0, Network
-	la $a1, Name2
-	jal is_person_name_exists 
+	la $a1, Name1
+	jal is_person_name_exists
+
 	#write test code
-	
+	move $s0, $v0
+	move $s1, $v1
+
+	move $a0, $s0
+	li $v0, 1
+	syscall
+
+	li $a0, '\n'
+	li $v0, 11
+	syscall
+
+	move $a0, $s1
+	li $v0, 1
+	syscall
+
+	li $a0, '\n'
+	li $v0, 11
+	syscall
+
+	la $a0, Network
+	li $v0, 1
+	syscall
+
 	li $v0, 10
 	syscall
 	
